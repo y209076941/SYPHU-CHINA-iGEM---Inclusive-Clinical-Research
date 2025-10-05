0713cb9af7bb78e66dbfd998ddcdef77e04f64e2
BenChan-6 <chenwenbincwb2006@gmail.com>
2025-10-05 13:05:04 +0800

    Update SYPHU-CHINA iGEM - Inclusive Clinical Research.py

--- SYPHU-CHINA iGEM - Inclusive Clinical Research.py
+++ SYPHU-CHINA iGEM - Inclusive Clinical Research.py
@@ -610,8 +610,8 @@ with tab1:
                     texts['communication_options']
                 )
 
-            col1, col2, col3 = st.columns([1, 1, 1])
-            with col2:
+            col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col312, col13, col14, col15, col16, col17, col18, col19 = st.columns([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+            with col10:
                 if st.form_submit_button(f"**{texts['next']}**"):
                     if questionnaire_id:
                         st.session_state.form_data.update({
@@ -680,8 +680,8 @@ with tab1:
                                                 ["Under 10k", "10k-30k", "30k-50k", "50k-100k", "Over 100k",
                                                  "Insurance Covered"])
 
-            col1, col2, col3 = st.columns([1, 1, 1])
-            with col2:
+            col1, col2, col3 = st.columns([1, 2, 1])
+            with col3:
                 if st.form_submit_button(f"**{texts['next']}**"):
                     st.session_state.current_step = 3
                     st.rerun()
